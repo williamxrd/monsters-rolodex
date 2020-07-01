db00c86b93654deb218325596debc05031bfeeea
William R <wxavier8@gmail.com>
2020-07-01 01:16:19 -0300

    Updates

---
+++ precache-manifest.1f48c62da473c7a5362501da06481de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd7752443ab5b2f0660ccc9a0729448",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a2bcbf8f8dc7fa4d730",
+    "url": "/monsters-rolodex/static/css/main.628205cd.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a2bcbf8f8dc7fa4d730",
+    "url": "/monsters-rolodex/static/js/main.cd14241a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
